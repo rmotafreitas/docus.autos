ba3abaf167a01d230d5f055218fd3d541febfd8a
Pedro Vieira <teknight.matsui.contact@gmail.com>
2023-10-11 14:32:07 +0100

    Change R

--- api/src/lib/opeanai.ts
+++ api/src/lib/opeanai.ts
@@ -1,5 +1,8 @@
 import { OpenAI } from "openai";
+import 'dotenv/config'
+
 
 export const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY, 
 });
+
