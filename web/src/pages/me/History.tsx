c1de60feda4aaca45f28620bda43d4305cca279f
RICARDO FREITAS <rmotafreitas@gmail.com>
2023-10-17 19:20:07 +0100

    Using a table in history page

--- web/src/pages/me/History.tsx
+++ web/src/pages/me/History.tsx
@@ -4,12 +4,31 @@ import { register } from "@teamhanko/hanko-elements";
 import { hankoInstance } from "@/lib/hanko";
 import { useNavigate } from "react-router-dom";
 import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 import { Navbar } from "@/components/navbar";
+import {
+  FileAudio,
+  FileText,
+  FileVideo,
+  FilterIcon,
+  Globe,
+} from "lucide-react";
 
 export function HistoryPage() {
   const hanko = useMemo(() => hankoInstance, []);
@@ -27,18 +46,61 @@ export function HistoryPage() {
   }, []);
 
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen justify-center items-center">
       <Navbar />
-      <section className="flex flex-col justify-center">
-        <Accordion type="single" collapsible className="border-white border-2">
-          <AccordionItem value="item-1">
-            <AccordionTrigger>Is it accessible?</AccordionTrigger>
-            <AccordionContent>
-              Yes. It adheres to the WAI-ARIA design pattern.
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
-      </section>
+      <div className="flex flex-col justify-center items-center flex-1 min-h-full min-w-[74rem] max-w-[74rem]">
+        <div className="flex justify-end w-full mb-4">
+          <DropdownMenu>
+            <DropdownMenuTrigger className="flex flex-row justify-center items-center gap-2">
+              <FilterIcon className="text-xl" />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>Media Filter</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>All</DropdownMenuItem>
+              <DropdownMenuItem className="flex flex-row gap-2 justify-start items-center">
+                <FileVideo className="w-4" />
+                Videos
+              </DropdownMenuItem>
+
+              <DropdownMenuItem className="flex flex-row gap-2 justify-start items-center">
+                <Globe className="w-4" />
+                Websites
+              </DropdownMenuItem>
+
+              <DropdownMenuItem className="flex flex-row gap-2 justify-start items-center">
+                <FileText className="w-4" />
+                Documents
+              </DropdownMenuItem>
+
+              <DropdownMenuItem className="flex flex-row gap-2 justify-start items-center">
+                <FileAudio className="w-4" />
+                Audios
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+
+        <Table>
+          <TableCaption>A list of your {} prompts.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Invoice</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Method</TableHead>
+              <TableHead className="text-right">Amount</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell className="font-medium">INV001</TableCell>
+              <TableCell>Paid</TableCell>
+              <TableCell>Credit Card</TableCell>
+              <TableCell className="text-right">$250.00</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
     </div>
   );
 }
